f0d92c80e6a6d6b45778337dcacd22efaaed753a
lution88 <lution88@gmail.com>
2022-01-27 21:21:29 +0900

    check files

--- user/models.py
+++ user/models.py
@@ -1,15 +1,20 @@
+from csv import unregister_dialect
 from django.db import models
 
-<<<<<<< HEAD
 # Create your models here.
-=======
 
-# # Create your models here.
-# class UserModel(models.Model):
-#     class Meta:
-#         db_table = "user"
-#
-#     # user_code = models.IntegerField(primary_key=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
->>>>>>> c96397438d61241119e61f84486e6c868410a41c
+class UserModel(models.Model):
+    class Meta:
+        db_table = "user"
+    
+    user_code = models.AutoField(primary_key=True)
+    user_id = models.CharField(max_length=20)
+    password = models.CharField(max_length=50)
+    create_at = models.DateTimeField(auto_now_add=True)
+    email = models.CharField(max_length=50)
+    pro_img_url = models.CharField(max_length=255)
+    play_game = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user_id
+
