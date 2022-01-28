c28d8b8aa34c2c8d02a242276e1a1e55d911f6a0
lution88 <lution88@gmail.com>
2022-01-28 10:46:49 +0900

    update db

--- stgames/settings.py
+++ stgames/settings.py
@@ -75,11 +75,15 @@ WSGI_APPLICATION = 'stgames.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+DATABASES = { 
+    'default': 
+    { 'ENGINE': 'django.db.backends.mysql', 
+    'NAME': 'redeyes1234', 
+    'USER': 'redeyes1234', 
+    'PASSWORD': 'python!1004', 
+    'PORT': '3306', 
+    'HOST': 'umj7-015.cafe24.com', 
+    } 
 }
 
 
