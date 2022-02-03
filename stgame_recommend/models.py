2e858b52a72553ff5f02f7e274e0edf80e12b293
asd <pa6712@naver.com>
2022-02-03 14:08:27 +0900

    초기화

--- stgame_recommend/models.py
+++ stgame_recommend/models.py
@@ -8,3 +8,11 @@ class UserModel(AbstractUser):
 
     nickname = models.CharField(max_length=256, default='')
 
+
+class Games(models.Model):
+    class Meta:
+        db_table = 'games'
+
+    game = models.CharField(max_length=256, default='')
+    user_id = models.CharField(max_length=256, default='')
+    playtime = models.DateTimeField(blank=True, null=True)
