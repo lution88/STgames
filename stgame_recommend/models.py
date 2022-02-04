06afb4ccc27ead15301b84ddb192933e51efb0ac
lution88 <lution88@gmail.com>
2022-02-04 11:29:13 +0900

    update modles.py

--- stgame_recommend/models.py
+++ stgame_recommend/models.py
@@ -7,6 +7,7 @@ class UserModel(AbstractUser):
         db_table = 'user'
 
     nickname = models.CharField(max_length=256, default='')
+    pro_img_url = models.CharField(max_length=256, default='')
 
 
 class Games(models.Model):
@@ -14,5 +15,15 @@ class Games(models.Model):
         db_table = 'games'
 
     game = models.CharField(max_length=256, default='')
+    tag = models.CharField(max_length=256, default='')
+    game_id = models.CharField(max_length=256, default='')
+    game_url = models.CharField(max_length=256, default='')
+
+
+class FavoriteGames(models.Model):
+    class Meta:
+        db_table = 'favoritegames'
+
+    game_id = models.CharField(max_length=256, default='')
     user_id = models.CharField(max_length=256, default='')
     playtime = models.DateTimeField(blank=True, null=True)
