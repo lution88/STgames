228fa338bf1c778584598be0b40e4db2855aa740
lution88 <lution88@gmail.com>
2022-02-04 11:38:46 +0900

    update tests.py urls.py

--- stgame_recommend/tests.py
+++ stgame_recommend/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from stgame_recommend.models import UserModel
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        UserModel.objects.create(name="lion", sound="roar")
+        UserModel.objects.create(name="cat", sound="meow")
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = UserModel.objects.get(name="lion")
+        cat = UserModel.objects.get(name="cat")
+        self.assertEqual(lion.speak(), 'The lion says "roar"')
+        self.assertEqual(cat.speak(), 'The cat says "meow"')
