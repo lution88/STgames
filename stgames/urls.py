1f49980dd959649b01b0526a32e793f1ef86c937
yongheon-Lee <dydgjs778@gmail.com>
2022-02-04 15:22:12 +0900

    수정: url별 명칭 추가

--- stgames/urls.py
+++ stgames/urls.py
@@ -18,14 +18,30 @@ from django.urls import path,include
 from stgame_recommend import views
 
 urlpatterns = [
+    # 관리자 페이지 url
     path('admin/', admin.site.urls),
+
+    # 하위 앱 url파일 연결
     path('', include('stgame_recommend.urls')),
+
+    # 시작 페이지 url
     path('', views.index, name='index'),
 
+    # 로그인 url
     path('sign-in/', views.sign_in, name='sign-in'),
+
+    # 회원가입 url
     path('sign-up/', views.sign_up, name='sign-up'),
+
+    # 아이디 중복 확인 url
     path('id-check/', views.id_check, name='id-check'),
+
+    # 이메일 중복 확인 url
     path('email-check/', views.email_check, name='email-check'),
+
+    # 메인 페이지 url
     path('main/', views.main, name='main'),
+
+    # 마이 페이지 url
     path('my-page/', views.my_page, name='my-page'),
 ]
