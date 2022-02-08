66b8668d756f89c8bd2661f4ac84cc2c326fa500
yongheon-Lee <dydgjs778@gmail.com>
2022-02-08 17:37:33 +0900

    수정: aws s3 연동 json 파일 경로 수정

--- stgames/settings.py
+++ stgames/settings.py
@@ -161,7 +161,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 with open(os.path.join(BASE_DIR, 'stgames/config/aws.json')) as f:
     secrets = json.loads(f.read())
 
-AWS_ACCESS_KEY_IE = secrets['AWS']['ACCESS_KEY_ID']
+AWS_ACCESS_KEY_ID = secrets['AWS']['ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS']['SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS']['STORAGE_BUCKET_NAME']
 AWS_DEFAULT_ACL = 'public-read'
